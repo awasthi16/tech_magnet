d885555c876192daea1275439ad2aaeb26bef5a0
durgesh awasthi <durgesh1000awasthi@gmail.com>
2025-11-06 08:56:21 +0530

    new

---
+++ src/Component/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+
+  <div className='subhead'>
+
+
+<div className='icon'>
+
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0078FF" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+</svg>
+</div>
+
+
+<div className='logo'><h1>SERP Analytics Dashboard</h1>
+<p>DataForSEO API Integration
+
+</p>
+</div>
+  </div>
+
+    </div>
+  )
+}
+
+export default Header
