dc8417a7266748b9822d713c2846cacb1549f0d3
durgesh awasthi <durgesh1000awasthi@gmail.com>
2025-11-08 09:08:14 +0530

    language

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL || "https://tech-maganet-backend.vercel.app",
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL || "https://tech-maganet-backend.vercel.app",
+    // baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
 });
 
 export default api;
