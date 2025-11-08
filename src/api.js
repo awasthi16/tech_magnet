439e0ebe797195fd775794982202096e53a42ebf
durgesh awasthi <durgesh1000awasthi@gmail.com>
2025-11-08 09:43:19 +0530

    add

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://tech-maganet-backend.vercel.app",
-    // baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "https://tech-maganet-backend.vercel.app",
+  // 👇 optional timeout to handle network delays
+  // timeout: 15000,
 });
 
 export default api;
-
-
-// http://localhost:5000
